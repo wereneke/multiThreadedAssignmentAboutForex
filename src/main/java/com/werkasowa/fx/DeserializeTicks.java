3cf480424f8aecbb0c5c4e5f5bef6dcbb8f1411d
wereneke <weronikad752@gmail.com>
2018-02-27 14:01:58 +0100

    create DeserializeTicks class

---
+++ src/main/java/com/werkasowa/fx/DeserializeTicks.java
@@ -0,0 +1,32 @@
+package com.werkasowa.fx;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class DeserializeTicks {
+
+    Gson gson = new Gson();
+    JsonParser parser = new JsonParser();
+
+    public Tick tick(String json) {
+
+        Collection collection = new ArrayList();
+        JsonArray array = parser.parse(json).getAsJsonArray();
+        for (JsonElement o: array) {
+            collection.add(gson.fromJson(o, String.class));
+        }
+
+        return new Tick();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
